1407c8a18c60bae502a6200c2639c1b176d47429
MrXmit <tonixsmit@gmail.com>
2023-10-31 15:27:28 +0200

    implement Delete Finch - start Edit finch

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,30 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Finch
+# from .forms import FeedingForm
+
+
+class FinchCreate(CreateView):
+  model = Finch
+  fields = ['name', 'breed', 'description', 'age']
+  success_url = '/finches/'
+
+class FinchUpdate(UpdateView):
+  model = Finch
+  # Let's disallow the renaming of a finch by excluding the name field!
+  fields = ['breed', 'description', 'age']
+
+class FinchDelete(DeleteView):
+  model = Finch
+  success_url = '/finches/' 
+
+# def add_feeding(request, finch_id):
+#   form = FeedingForm(request.POST)
+#   if form.is_valid():
+#     new_feeding = form.save(commit=False)
+#     new_feeding.finch_id = finch_id
+#     new_feeding.save()
+#   return redirect('finch-detail', finch_id=finch_id)
 
 # Define the home view
 def home(request):
