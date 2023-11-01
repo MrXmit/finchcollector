787640d7e0673169eb5bf6de064c1eef3c083702
MrXmit <tonixsmit@gmail.com>
2023-11-01 13:56:36 +0200

    finish part 4

--- main_app/models.py
+++ main_app/models.py
@@ -1,5 +1,11 @@
 from django.db import models
 
+MEALS = (
+  ('B', 'Breakfast'),
+  ('L', 'Lunch'),
+  ('D', 'Dinner')
+)
+
 class Finch(models.Model):
   name = models.CharField(max_length=100)
   breed = models.CharField(max_length=100)
@@ -8,3 +14,20 @@ class Finch(models.Model):
 
   def __str__(self):
     return self.name
+  
+class Feeding(models.Model):
+  date = models.DateField('Feeding Date')
+  meal = models.CharField(
+    max_length=1,
+    choices=MEALS,
+    default=MEALS[0][0]
+  )
+  # Create a finch_id column in the database
+  finch = models.ForeignKey(Finch, on_delete=models.CASCADE)
+
+  def __str__(self):
+    # Nice method for obtaining the friendly value of a Field.choice
+    return f"{self.get_meal_display()} on {self.date}"
+  
+  class Meta:
+    ordering = ['-date']
--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Finch
-# from .forms import FeedingForm
-
+from .forms import FeedingForm
 
 class FinchCreate(CreateView):
   model = Finch
@@ -18,13 +17,14 @@ class FinchDelete(DeleteView):
   model = Finch
   success_url = '/finches/' 
 
-# def add_feeding(request, finch_id):
-#   form = FeedingForm(request.POST)
-#   if form.is_valid():
-#     new_feeding = form.save(commit=False)
-#     new_feeding.finch_id = finch_id
-#     new_feeding.save()
-#   return redirect('finch-detail', finch_id=finch_id)
+  
+def add_feeding(request, finch_id):
+  form = FeedingForm(request.POST)
+  if form.is_valid():
+    new_feeding = form.save(commit=False)
+    new_feeding.finch_id = finch_id
+    new_feeding.save()
+  return redirect('finch-detail', finch_id=finch_id)
 
 # Define the home view
 def home(request):
@@ -40,4 +40,5 @@ def finch_index(request):
 
 def finch_detail(request, finch_id):
   finch = Finch.objects.get(id=finch_id)
-  return render(request, 'finches/detail.html', { 'finch': finch })
+  feeding_form = FeedingForm()
+  return render(request, 'finches/detail.html', { 'finch': finch, 'feeding_form': feeding_form})
