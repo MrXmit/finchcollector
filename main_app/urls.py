34d7a23c94bee7574a0ef86230535266390aad87
MrXmit <tonixsmit@gmail.com>
2023-11-02 13:12:12 +0200

    implement part 7 Auth

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.Home.as_view(), name='home'),
+    path('accounts/signup/', views.signup, name='signup'),
     path('about/', views.about, name='about'),
     path('finches/', views.finch_index, name='finch-index'),
     path('finches/<int:finch_id>/', views.finch_detail, name='finch-detail'),
--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render, redirect
 from django.views.generic import ListView, DetailView
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required 
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Finch, Toy
 from .forms import FeedingForm
+from django.contrib.auth.views import LoginView
+
+class Home(LoginView):
+  template_name = 'home.html'
 
 class ToyCreate(CreateView):
   model = Toy
@@ -27,16 +34,19 @@ class FinchCreate(CreateView):
   fields = ['name', 'breed', 'description', 'age']
   success_url = '/finches/'
 
+  def form_valid(self, form):
+    form.instance.user = self.request.user
+    return super().form_valid(form)
+
 class FinchUpdate(UpdateView):
   model = Finch
-  # Let's disallow the renaming of a finch by excluding the name field!
   fields = ['breed', 'description', 'age']
 
 class FinchDelete(DeleteView):
   model = Finch
   success_url = '/finches/' 
 
-  
+@login_required
 def add_feeding(request, finch_id):
   form = FeedingForm(request.POST)
   if form.is_valid():
@@ -45,25 +55,39 @@ def add_feeding(request, finch_id):
     new_feeding.save()
   return redirect('finch-detail', finch_id=finch_id)
 
-# Define the home view
 def home(request):
   return render(request, 'home.html')
 
 def about(request):
   return render(request, 'about.html')
 
-# Add new view
+@login_required
 def finch_index(request):
-  finches = Finch.objects.all()
+  finches = Finch.objects.filter(user=request.user)
   return render(request, 'finches/index.html', { 'finches': finches })
 
+@login_required
 def finch_detail(request, finch_id):
   finch = Finch.objects.get(id=finch_id)
   toys_finch_doesnt_have = Toy.objects.exclude(id__in = finch.toys.all().values_list('id'))
   feeding_form = FeedingForm()
   return render(request, 'finches/detail.html', { 'finch': finch, 'feeding_form': feeding_form, 'toys': toys_finch_doesnt_have})
 
+@login_required
 def assoc_toy(request, finch_id, toy_id):
-  # Note that you can pass a toy's id instead of the whole object
   Finch.objects.get(id=finch_id).toys.add(toy_id)
   return redirect('finch-detail', finch_id=finch_id)
+
+def signup(request):
+  error_message = ''
+  if request.method == 'POST':
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+      user = form.save()
+      login(request, user)
+      return redirect('finch-index')
+    else:
+      error_message = 'Invalid sign up - try again'
+  form = UserCreationForm()
+  context = {'form': form, 'error_message': error_message}
+  return render(request, 'signup.html', context)
